b03a0f800b0bef2c873d9f0b8d354fb986a23d9a
waqas-ahmd <65waqas@gmail.com>
2023-10-28 12:33:59 +0500

    landing page header

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,229 @@
-import Image from 'next/image'
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { Lora } from "next/font/google";
+import React from "react";
+
+const lora = Lora({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
 
 export default function Home() {
+  const [vehicle, setVehicle] = React.useState(0);
+  const [paymentType, setPaymentType] = React.useState("Buy");
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <main className="flex min-h-screen flex-col items-center justify-start">
+      <nav className="p-3 flex flex-col items-center bg-white w-full">
+        <div className="max-w-5xl w-full flex flex-row justify-between">
+          <Link href="/">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              src="/assets/images/Logo.png"
+              className="h-14 object-contain"
+              width={160}
+              height={80}
+              alt="Logo"
             />
-          </a>
+          </Link>
+
+          <div className="flex flex-row gap-6 items-center">
+            <div className="flex flex-row gap-1 items-center">
+              Search
+              <Image
+                src="/assets/icons/arrow_down.svg"
+                alt="Arrow"
+                height={18}
+                width={18}
+              />
+            </div>
+            <div>News & Guides</div>
+            <button className="bg-red py-2 px-8 text-white rounded-lg">
+              Sell
+            </button>
+          </div>
+
+          <div className="flex flex-row gap-6 items-center">
+            <div className="flex flex-row gap-1 items-center">
+              <Image
+                src="/assets/icons/bell.svg"
+                alt="Arrow"
+                height={24}
+                width={24}
+              />
+              <Image
+                src="/assets/icons/arrow_down.svg"
+                alt="Arrow"
+                height={18}
+                width={18}
+              />
+            </div>
+            <Image
+              src="/assets/icons/star.svg"
+              alt="Arrow"
+              height={24}
+              width={24}
+            />
+            <Image
+              src="/assets/icons/chat.svg"
+              alt="Arrow"
+              height={24}
+              width={24}
+            />
+            <button className="bg-blue py-2 px-8 text-white rounded-lg">
+              Login
+            </button>
+          </div>
         </div>
-      </div>
+      </nav>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+      <section className="w-full px-3 flex flex-col items-center relative">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src="/assets/images/Pozadina.png"
+          width={1400}
+          height={500}
+          alt="Cars"
+          className="absolute top-0 left-0 h-full max-h-[300px] w-full object-cover"
         />
-      </div>
+        <div className="w-full max-w-5xl py-20">
+          <div className="relative w-fit">
+            <div className="absolute top-0 left-0 w-full h-full bg-white z-0 blur-xl"></div>
+            <div className="relative z-[1]">
+              <p className={"text-5xl font-semibold " + lora.className}>
+                Find Your <span className="text-red">Dream Car</span> Here
+              </p>
+              <p className="text-[#444444] text-lg">Search from 70,862 cars</p>
+            </div>
+          </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <div className="rounded-2xl bg-white shadow-lg overflow-hidden w-full relative z-20 mt-8">
+            <div className="flex flex-row w-full">
+              {["car", "motorcycle", "bicycle", "truck", "van", "boat"].map(
+                (v, i) => (
+                  <div
+                    onClick={() => setVehicle(i)}
+                    className={`py-2 flex-1 flex flex-col justify-center items-center cursor-pointer ${
+                      vehicle === i ? "bg-white" : "bg-[#EBEBEB]"
+                    }`}
+                    key={i}
+                  >
+                    <Image
+                      src={`/assets/icons/${v}_${
+                        vehicle === i ? "red" : "black"
+                      }.svg`}
+                      height={30}
+                      width={60}
+                      className="w-7"
+                      alt="car"
+                    />
+                  </div>
+                )
+              )}
+            </div>
+            <div className="p-4 grid grid-cols-4 gap-4">
+              <div className="col-span-3 grid grid-cols-4 gap-2">
+                <div className="col-span-3 flex flex-col gap-2">
+                  <div className="grid grid-cols-3 gap-2">
+                    <select className="border border-[#B8B8B8] rounded-lg p-2">
+                      <option>Make</option>
+                    </select>
+                    <select className="border border-[#B8B8B8] rounded-lg p-2">
+                      <option>Model</option>
+                    </select>
+                    <input
+                      className="border border-[#B8B8B8] rounded-lg p-2"
+                      placeholder="Variant"
+                    />
+                  </div>
+                  <div className="grid grid-cols-4 gap-2">
+                    <select className="border border-[#B8B8B8] rounded-lg p-2">
+                      <option>Price From</option>
+                    </select>
+                    <select className="border border-[#B8B8B8] rounded-lg p-2">
+                      <option>Price Up to</option>
+                    </select>
+                    <select className="border border-[#B8B8B8] rounded-lg p-2">
+                      <option>Year From</option>
+                    </select>
+                    <select className="border border-[#B8B8B8] rounded-lg p-2">
+                      <option>Year Up to</option>
+                    </select>
+                  </div>
+                  <div className="grid grid-cols-4 gap-2">
+                    <select className="border border-[#B8B8B8] rounded-lg p-2 col-span-1">
+                      <option>Fuel Type</option>
+                    </select>
+                    <div className="border border-[#B8B8B8] rounded-lg col-span-3 flex flex-row items-center">
+                      <p className="flex-1 p-2">Payment Type</p>
+                      <div
+                        onClick={() => setPaymentType("Buy")}
+                        className={`p-2 flex-1 text-center rounded-lg cursor-pointer ${
+                          paymentType === "Buy"
+                            ? "bg-blue border border-blue text-white"
+                            : "text-blue"
+                        } font-medium`}
+                      >
+                        Buy
+                      </div>
+                      <div
+                        onClick={() => setPaymentType("Leasing")}
+                        className={`p-2 flex-1 text-center rounded-lg cursor-pointer ${
+                          paymentType === "Leasing"
+                            ? "bg-blue border border-blue text-white"
+                            : "text-blue"
+                        } font-medium`}
+                      >
+                        Leasing
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-span-1 flex flex-col gap-2">
+                  <button className="bg-blue border border-blue text-white py-2 px-1 tracking-tight rounded-lg flex flex-row items-center justify-center gap-2">
+                    + Add another vehicle
+                  </button>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+                  <select
+                    className="border border-[#B8B8B8] rounded-lg p-2"
+                    placeholder="Vehicle Type"
+                  >
+                    <option>Vehicle Type</option>
+                  </select>
+                  <input
+                    className="border border-[#B8B8B8] rounded-lg p-2"
+                    placeholder="City or Zip Code"
+                  />
+                </div>
+              </div>
+              <div className="col-span-1 flex flex-col gap-2">
+                <button className="bg-red border border-red text-white py-2 px-8 rounded-lg flex flex-row items-center justify-center gap-2">
+                  <Image
+                    src="/assets/icons/search.svg"
+                    height={24}
+                    width={24}
+                    alt="search"
+                    className="h-4 w-4"
+                  />{" "}
+                  5.221.456 results
+                </button>
+                <button className="border border-blue text-blue rounded-lg py-2 px-8">
+                  Save Search
+                </button>
+                <button className="border border-blue text-blue rounded-lg py-2 px-8 flex flex-row items-center justify-center gap-2">
+                  More filters{" "}
+                  <Image
+                    src="/assets/icons/arrow_down.svg"
+                    height={24}
+                    width={24}
+                    alt="search"
+                    className="h-4 w-4"
+                  />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
-  )
+  );
 }
